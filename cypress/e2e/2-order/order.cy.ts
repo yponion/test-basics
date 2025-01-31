2f990be019079ebc6a85cb435600c8cb7d0107e3
yponion <yp071704@gmail.com>
2025-01-31 21:15:53 +0900

    feat: cypress로 공용 컴포넌트 테스트
    - 컴포넌트에 props로 내려줘서 사용

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('주문을 테스트 한다', () => {
+  it('사용자는 배달/포장 중 원하는 유형을 선택할 수 있다', () => {
+    cy.visit('/')
+    cy.get('[data-cy=deliveryBtn]').should("be.visible").as('deliveryBtn');
+    cy.get('[data-cy=pickupBtn]').should("be.visible").as('pickupBtn');
+    cy.get('@deliveryBtn').click();
+    cy.url().should('include', '/food-type');
   })
 })
