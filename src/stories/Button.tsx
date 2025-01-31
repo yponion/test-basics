fdd59e97f7729f1acc1cb4d369b9336f9b1f9b46
yponion <yp071704@gmail.com>
2025-02-01 01:14:40 +0900

    feat: 스토리를 활용한 컴포넌트 작성

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './button.css';
+import "./button.css";
+import "../App.css";
 
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
@@ -8,27 +9,35 @@ export interface ButtonProps {
   /** What background color to use */
   backgroundColor?: string;
   /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   /** Button contents */
   label: string;
   /** Optional click handler */
   onClick?: () => void;
+  /** Optional disabled */
+  disabled?: boolean;
 }
 
 /** Primary UI component for user interaction */
 export const Button = ({
   primary = false,
-  size = 'medium',
+  disabled = true,
+  size = "medium",
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
       style={{ backgroundColor }}
+      disabled={disabled}
       {...props}
     >
       {label}
